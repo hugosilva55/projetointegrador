330928ed92c7aab9cbf0ed798d4796d9dd3fe065
Jonathan Costa Matos <37823427+JonathanMatos97@users.noreply.github.com>
2018-05-19 01:05:33 -0300

    Design das Tela de Login Feita

---
+++ Projeto/build/generated-sources/ap-source-output/br/com/Modelos/Funcionario_.java
@@ -0,0 +1,19 @@
+package br.com.Modelos;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Funcionario.class)
+public abstract class Funcionario_ {
+
+	public static volatile SingularAttribute<Funcionario, String> loginFunc;
+	public static volatile SingularAttribute<Funcionario, Integer> idUsuario;
+	public static volatile SingularAttribute<Funcionario, String> cpf;
+	public static volatile SingularAttribute<Funcionario, String> nomeFuncionario;
+	public static volatile SingularAttribute<Funcionario, String> senhaFunc;
+	public static volatile SingularAttribute<Funcionario, String> email;
+
+}
+
