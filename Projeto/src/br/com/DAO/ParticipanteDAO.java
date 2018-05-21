551fee80a89beac9f8b24986296e41b78f0bdfd7
ThiagoAlencar12 <37818718+ThiagoAlencar12@users.noreply.github.com>
2018-05-21 16:04:14 -0300

    Participante
    Ainda nao foi feito a tela, só as classes, a noite a tela sai

---
+++ Projeto/src/br/com/DAO/ParticipanteDAO.java
@@ -0,0 +1,16 @@
+package br.com.DAO;
+
+import br.com.Modelos.Participante;
+import br.com.Util.GenericDAO;
+
+/**
+ *
+ * @author Thiago Alencar
+ */
+public class ParticipanteDAO extends GenericDAO<Participante> {
+
+    public ParticipanteDAO() {
+        super(Participante.class);
+    }
+
+}
---
+++ Projeto/src/br/com/Modelos/Participante.java
@@ -0,0 +1,195 @@
+package br.com.Modelos;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Thiago Alencar
+ */
+@Entity
+public class Participante {
+
+    @Id
+    @GeneratedValue
+    private int idUsuario;
+    @Column(length = 50, nullable = false)
+    private String nome;
+    @Column(length = 50, nullable = false)
+    private String email;
+    @Column(length = 50, nullable = false)
+    private String estadoCivil;
+    @Column(length = 50, nullable = false)
+    private String CPF;
+    @Column(length = 50, nullable = false)
+    private String dataNascimento;
+    @Column(length = 50, nullable = false)
+    private String sexo;
+    @Column(length = 50, nullable = false)
+    private String nacionalidade;
+    @Column(length = 50, nullable = false)
+    private String RG;
+    @Column(length = 50, nullable = false)
+    private String nomePai;
+    @Column(length = 50, nullable = false)
+    private String nomeMae;
+
+    /**
+     * @return the idUsuario
+     */
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    /**
+     * @param idUsuario the idUsuario to set
+     */
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the estadoCivil
+     */
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    /**
+     * @param estadoCivil the estadoCivil to set
+     */
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    /**
+     * @return the CPF
+     */
+    public String getCPF() {
+        return CPF;
+    }
+
+    /**
+     * @param CPF the CPF to set
+     */
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    /**
+     * @return the dataNascimento
+     */
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    /**
+     * @param dataNascimento the dataNascimento to set
+     */
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public String getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the nacionalidade
+     */
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    /**
+     * @param nacionalidade the nacionalidade to set
+     */
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    /**
+     * @return the RG
+     */
+    public String getRG() {
+        return RG;
+    }
+
+    /**
+     * @param RG the RG to set
+     */
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    /**
+     * @return the nomePai
+     */
+    public String getNomePai() {
+        return nomePai;
+    }
+
+    /**
+     * @param nomePai the nomePai to set
+     */
+    public void setNomePai(String nomePai) {
+        this.nomePai = nomePai;
+    }
+
+    /**
+     * @return the nomeMae
+     */
+    public String getNomeMae() {
+        return nomeMae;
+    }
+
+    /**
+     * @param nomeMae the nomeMae to set
+     */
+    public void setNomeMae(String nomeMae) {
+        this.nomeMae = nomeMae;
+    }
+    
+    
+
+}
